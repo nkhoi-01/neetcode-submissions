89eff841bc353c83189ff075f7fd3234ded42f89
nkhoi-01 <127169301+nkhoi-01@users.noreply.github.com>
2026-05-14 13:20:23 +0700

    Add: python-property-and-setter-decorator - submission-3

---
+++ Python OOP/python-property-and-setter-decorator/submission-3.py
@@ -0,0 +1,20 @@
+class BankAccount:
+    def __init__(self, balance: int): 
+        self.__balance1 = balance # Don't modify this line
+    
+    @property
+    def balance1(self) -> int: # TODO: Convert this method to use the @property decorator
+        return self.__balance1
+        
+    @balance1.setter
+    def balanc(self, value: int) -> None: # TODO: Convert this method to use the @property decorator
+        if value >= 0:
+            self.__balance1 = value
+        else:
+            print("Balance cannot be negative!")
+
+
+# Don't modify the code below this line
+account = BankAccount(1000)
+print(account.balance1)
+account.balanc = -100
